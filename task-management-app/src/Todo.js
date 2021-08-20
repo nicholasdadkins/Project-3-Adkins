83bc39d798f518c62548bf99c1a5e48b43dba890
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-08-20 17:12:04 -0400

    Added delete feature

--- task-management-app/src/Todo.js
+++ task-management-app/src/Todo.js
@@ -1,36 +1,61 @@
 import React, {useState, useEffect} from 'react';
 
-// 1. Get information from the input element. So when the user types in something and clicks on add button, you should have access to what was typed.
-// 2. Display OR add the item to the list
+// This is the only one that is left
 // 3. Show All, Active and completed
 
 function Todo(props) {
 
-       // const [toDoList, setToDoList] = useState(['']);
-        const [toDoList, setToDoList] = useState(['Item one', 'Item two']);
-        const [itemText, setItemText] = useState('');
-        const updateInputValue = async (event) => {
-            console.log(event.currentTarget.value)
-            setItemText(event.currentTarget.value)
-        };
+    const [newItemText, setNewItemText] = useState('');
+    const [items, setItems] = useState([
+        {text: 'Go to the grocery', completed: false},
+        {text: 'Work on project', completed: false},
+        {text: 'Study Programming', completed: false}
+    ]);
 
-        const addItem = async () => {
-            setToDoList([...toDoList, itemText])
-            setItemText('')
-        };
+    const onTextChange = async (event) => {
+        setNewItemText(event.currentTarget.value)
+    };
 
-   
-        
-        return (
+    const addItem = async () => {
+        setItems([...items, {text: newItemText, completed: false}])
+        setNewItemText('')
+    };
+
+    const onCheckBoxChange = async (event, item, idx) => {
+        let itemsCopy = items
+        itemsCopy.map((itemCopy, index) => {
+            if(index == idx){
+                itemCopy.completed = event.target.checked
+            }
+        })
+        setItems([...itemsCopy])
+    };
+
+    const onItemDeleted = async (event, item, idx) => {
+        let itemsCopy = []
+        items.map((item, index) => {
+            if(index != idx){
+                itemsCopy.push(item)
+            }
+        })
+        setItems([...itemsCopy])
+    };
+
+    return (
         <>
-            <div className={'flex justify-center mt-20'}>
+        <div className={'flex justify-center mt-20'}>
                 <div className={' h-96 w-1/3'}>
 
 
                     <div className={'flex p-4'}>
                         <div className={'w-2/3'}>
-                            <input value={itemText} onChange={updateInputValue} type="text" placeholder={'Add task...'} className={'border p-2 w-full'}/>
+                            <input value={newItemText}
+                                   onChange={onTextChange}
+                                   placeholder={'Add task...'}
+                                   type="text"
+                                   className={'border p-2 w-full'}/>
                         </div>
+
                         <div className={'w-1/3 flex justify-center'}>
                             <button onClick={addItem} className={'border-2 border-blue-500 text-blue-500 font-bold rounded w-2/3 mr-4'}>Add</button>
                         </div>
@@ -39,17 +64,22 @@ function Todo(props) {
 
 
                     <div className={'bg-gray-100 h-full p-2 mt-4'}>
+
                         <ul>
-                         {toDoList.map((item, idx) => {
-                              return <li className={'flex relative p-2 border-b-2 border-blue-100'} key={idx}>
-                                <div className={'mr-4'}><input type="checkbox"/></div>  
-                                <div className={''}>
-                                    {item}
-                                    <i className={'fa fa-trash absolute right-10 text-red-300'}/>
-                                </div>
-                            </li>
-                          })}
+
+                            {items.map((item, idx) => {
+                               return <li className={'flex relative p-2 border-b-2 border-blue-100'}>
+                                   <div className={'mr-4'}>
+                                       <input type="checkbox" onChange={(event) => onCheckBoxChange(event, item, idx)}/>
+                                   </div>
+                                   <div className={''}>
+                                       {item.text}
+                                       <i onClick={(event) => onItemDeleted(event, item, idx)} className={'fa fa-trash absolute right-10 text-red-300 cursor-pointer'}/>
+                                   </div>
+                               </li>
+                            })}
                         </ul>
+
                     </div>
 
                     <div className={'mt-4'}>
