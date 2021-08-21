e3da211d3900e566d740c1797417c75298dd06cd
nicholasdadkins <nicholasdonadkins@gmail.com>
2021-08-21 14:11:13 -0400

    Made link style active

--- task-management-app/src/App.js
+++ task-management-app/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {useState, useEffect} from 'react'
-import MyButton from "./my-button";
-import MenuItem from "./menu-item"; //Both of these are just hooks
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    NavLink
 } from "react-router-dom";
 import About from './About'
 import Todo from "./Todo";
@@ -15,25 +12,19 @@ import Contact from "./Contact";
 
 function App() {
 
-    // useEffect(() => {
-    //     console.log('run this code only on page load')
-    //     // console.log('run this code only when certain things in the deps list changes')
-    // }, [])
-    
   return (
-
       <Router>
           <div>
               <nav className={'border p-2 bg-gray-900'}>
                   <ul>
                       <li className={'inline p-2 text-white'}>
-                          <Link to="/">About</Link>
+                          <NavLink exact activeClassName="active" to="/about">About</NavLink>
                       </li>
-                      <li className={'inline p-2 bg-blue-600 text-white'}>
-                          <Link to="/todo">Todo</Link>
+                      <li className={'inline p-2  text-white'}>
+                          <NavLink exact activeClassName="active" to="/todo">Todo</NavLink>
                       </li>
                       <li className={'inline p-2 text-white'}>
-                          <Link to="/contact">Contact</Link>
+                          <NavLink exact activeClassName="active" to="/contact">Contact</NavLink>
                       </li>
                   </ul>
               </nav>
--- task-management-app/src/Todo.js
+++ task-management-app/src/Todo.js
@@ -7,10 +7,13 @@ function Todo(props) {
 
     const [newItemText, setNewItemText] = useState('');
     const [items, setItems] = useState([
-        {text: 'Go to the grocery', completed: false},
-        {text: 'Work on project', completed: false},
-        {text: 'Study Programming', completed: false}
+        {text: 'Go to the grocery store', completed: false},
+        {text: 'Feed pets', completed: false},
+        {text: 'Garden', completed: false}
     ]);
+
+    const [allItem, setAllItems] = useState([]);
+
 
     const onTextChange = async (event) => {
         setNewItemText(event.currentTarget.value)
@@ -18,7 +21,50 @@ function Todo(props) {
 
     const addItem = async () => {
         setItems([...items, {text: newItemText, completed: false}])
+        setAllItems([...items, {text: newItemText, completed: false}])
         setNewItemText('')
+    };
+    
+    const allItems = async () => {
+        //setItems([...items, {text: newItemText, completed: false}])
+        let itemsAll = allItem
+        setItems([...itemsAll])
+        setNewItemText('all')
+    };
+
+    const filterCompleted = [
+        {
+          predicateFn: allItem => allItem.completed == "true"
+        }
+      ];
+    
+      function getFilteredCompleted(filterCompleted) {
+        return allItem.filter(p => filterCompleted.every(filter => filter.predicateFn(p)));
+      }
+
+    const activeItems = async () => {
+        let itemsActive = getFilteredCompleted(filterCompleted)
+
+    
+        // itemsCopy.map((itemCopy, index) => {
+        //     if(index == idx){
+        //         itemCopy.completed = event.target.checked
+        //     }
+        // })
+        setItems([...itemsActive])
+        setNewItemText('active')
+    };
+
+    const completedItems = async () => {
+        let itemsCopy = items.filter(items => items.completed == true)
+    
+        // itemsCopy.map((itemCopy, index) => {
+        //     if(index == idx){
+        //         itemCopy.completed = event.target.checked
+        //     }
+        // })
+        setItems([...itemsCopy])
+        setNewItemText('completed')
     };
 
     const onCheckBoxChange = async (event, item, idx) => {
@@ -62,11 +108,8 @@ function Todo(props) {
 
                     </div>
 
-
                     <div className={'bg-gray-100 h-full p-2 mt-4'}>
-
                         <ul>
-
                             {items.map((item, idx) => {
                                return <li className={'flex relative p-2 border-b-2 border-blue-100'}>
                                    <div className={'mr-4'}>
@@ -79,18 +122,15 @@ function Todo(props) {
                                </li>
                             })}
                         </ul>
-
                     </div>
 
                     <div className={'mt-4'}>
                         <h2 className={'font-semibold'}>Show</h2>
-                        <button className={'mt-2 mr-4 px-8 py-2 rounded bg-blue-500 text-white'}>All</button>
-                        <button className={'border mr-4 px-8 py-2 rounded bg-gray-200 text-gray-500 font-semibold'}>Active</button>
-                        <button className={'border px-8 py-2 rounded bg-gray-200 text-gray-500 font-semibold'}>Completed</button>
+                        <button onClick={allItems} className={'border-2 border-blue-500 text-blue-500 font-bold rounded w-2/3 mr-4'}>All</button>
+                        <button onClick={activeItems} className={'border-2 border-blue-500 text-blue-500 font-bold rounded w-2/3 mr-4'}>Active</button>
+                        <button onClick={completedItems} className={'border-2 border-blue-500 text-blue-500 font-bold rounded w-2/3 mr-4'}>Completed</button>
 
                     </div>
-
-
                 </div>
 
             </div>
